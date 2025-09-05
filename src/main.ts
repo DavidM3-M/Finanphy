a9d0bf46df671ee15a7e656e82a50a4090238abc
DavidMelo <jesusdavidm090@gmail.com>
2025-09-05 17:00:03 -0500

    CORS ACTIVATE 2

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 });
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
