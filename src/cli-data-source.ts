8f6eecc1968b1cb74cd287c020f02dc8c996c908
DavidMelo <jesusdavidm090@gmail.com>
2025-09-13 03:41:07 -0500

    Migration AddFiscalAdress to company

---
+++ src/cli-data-source.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  ssl: process.env.POSTGRES_SSL === 'true',
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+  migrationsTransactionMode: 'each',
+  synchronize: false,
+  logging: false,
+});
