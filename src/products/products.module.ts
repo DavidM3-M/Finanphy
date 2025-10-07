1407b3cafdbaa1a68372c3ae24611588cc786210
DavidMelo <jesusdavidm090@gmail.com>
2025-10-06 23:30:38 -0500

    Company Injection to productsmodule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,16 +1,16 @@
-// src/products/products.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { Company } from 'src/companies/entities/company.entity'; // ✅ Importás la entidad Company
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Product]), 
+    TypeOrmModule.forFeature([Product, Company]), // ✅ Registrás ambos repositorios
   ],
-  controllers: [ProductsController],     
-  providers: [ProductsService],         
-  exports: [TypeOrmModule],              
+  controllers: [ProductsController],
+  providers: [ProductsService],
+  exports: [TypeOrmModule], // ✅ Exportás el acceso a los repositorios
 })
 export class ProductsModule {}
