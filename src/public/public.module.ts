b0d89b0a7d406fc0e59304218efd26c03d4396a2
DavidMelo <jesusdavidm090@gmail.com>
2025-10-06 23:08:02 -0500

    Public module correction

--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PublicController } from './public.controller';
 import { ProductsModule } from 'src/products/products.module';
+import { PublicService } from './public.service';
 
 @Module({
   imports: [ProductsModule],
   controllers: [PublicController],
+  providers: [PublicService]
 })
 export class PublicModule {}
