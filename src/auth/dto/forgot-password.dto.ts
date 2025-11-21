0a46a05b501bb7a9bb74d1bc92d3ba3fdeae08ef
DavidMelo <jesusdavidm090@gmail.com>
2025-11-21 15:25:26 -0500

    validation pipes on password

--- src/auth/dto/forgot-password.dto.ts
+++ src/auth/dto/forgot-password.dto.ts
@@ -1,11 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "node_modules/class-validator/types";
+
 // dto/forgot-password.dto.ts
 export class ForgotPasswordDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
 
+
 // dto/reset-password.dto.ts
 export class ResetPasswordDto {
+  @IsString()
   token: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(8)
   newPassword: string;
+
 }
