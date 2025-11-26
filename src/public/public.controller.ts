cc20405e4ebcb4c7e62e7d7f396ea8d1575ffeee
DavidMelo <jesusdavidm090@gmail.com>
2025-11-26 14:37:17 -0500

    Catalogo publico correct

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { PublicService } from './public.service';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('public')
 export class PublicController {
   constructor(private readonly publicService: PublicService) {}
 
   // Catálogo público por empresa
-  @public()
+  @Public()
   @Get('products/company/:companyId')
   getCatalog(@Param('companyId', ParseUUIDPipe) companyId: string) {
     return this.publicService.getCatalogByCompany(companyId);
